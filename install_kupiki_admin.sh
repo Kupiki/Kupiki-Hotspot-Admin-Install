adb4980ff0d9d4c1202fc7959defacb1deb0cdc9
Pi Home Server <pihomeserver@gmail.com>
2018-01-23 22:11:05 +0100

    v0.1c

--- install_kupiki_admin.sh
+++ install_kupiki_admin.sh
@@ -9,6 +9,7 @@ LOGNAME="pihotspot_admin.log"
 # be sure to add a / at the end of the path
 LOGPATH="/var/log/"
 
+MY_IP=`ip -4 route get 8.8.8.8 | awk {'print $7'} | tr -d '\n'`
 
 check_returned_code() {
     RETURNED_CODE=$@
@@ -49,41 +50,67 @@ execute_command() {
 
 # Install packages
 apt-get install -y build-essential libfontconfig1 curl apt-transport-https
+check_returned_code $?
 # Install nodejs / npm
 #curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
 curl --silent https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+check_returned_code $?
+
 echo "deb https://deb.nodesource.com/$NODE_VERSION $DISTRO main" | tee /etc/apt/sources.list.d/nodesource.list
+check_returned_code $?
+
 echo "deb-src https://deb.nodesource.com/$NODE_VERSION $DISTRO main" | tee -a /etc/apt/sources.list.d/nodesource.list
+check_returned_code $?
 
 apt-get update
+check_returned_code $?
 
 apt-get install -y nodejs
+check_returned_code $?
+
 npm install -g gulp-cli node-gyp
+check_returned_code $?
+
 #npm install -g n
 #n 8.5.0
 # Install PhantomJS for Raspberry Pi
-wget https://github.com/fg2it/phantomjs-on-raspberry/blob/master/rpi-2-3/wheezy-jessie/v2.1.1/phantomjs_2.1.1_armhf.deb?raw=true
-mv "phantomjs_2.1.1_armhf.deb?raw=true" phantomjs_2.1.1_armhf.deb
-dpkg -i phantomjs_2.1.1_armhf.deb
+#wget https://github.com/fg2it/phantomjs-on-raspberry/blob/master/rpi-2-3/wheezy-jessie/v2.1.1/phantomjs_2.1.1_armhf.deb?raw=true
+#mv "phantomjs_2.1.1_armhf.deb?raw=true" phantomjs_2.1.1_armhf.deb
+#dpkg -i phantomjs_2.1.1_armhf.deb
+
 apt-mark hold phantomjs
+check_returned_code $?
+
 # Create dedicated user kupiki
 adduser --disabled-password --gecos "" kupiki
+check_returned_code $?
+
 # Clone Kupiki-Hotspot-Admin project
 cd /home/kupiki
 git clone https://github.com/kupiki/Kupiki-Hotspot-Admin-Backend.git
+check_returned_code $?
+
 chown -R kupiki:kupiki /home/kupiki/Kupiki-Hotspot-Admin-Backend
+check_returned_code $?
+
 # Start packages installation with npm
 # cd Kupiki-Hotspot-Admin
 su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin-Backend && export NODE_ENV= && npm install"
+check_returned_code $?
+
 # Rebuild node-sass for Raspberry Pi
 su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin-Backend && npm rebuild node-sass"
+check_returned_code $?
+
 # Configure database connection
 
 # Build project
-su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin-Backend && gulp build"
+#su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin-Backend && gulp build"
 # Install PM2
-#npm install -g pm2
+npm install -g pm2
+check_returned_code $?
+
 # Configure startup of Kupiki Admin
 #echo "
 #module.exports = {
@@ -107,23 +134,40 @@ su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin-Backend && gulp build"
 #su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin-Backend && pm2 restart 0"
 #su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin-Backend && pm2 start npm -- run --env production"
 
+sed -i "s/192.168.10.160/$MY_IP/g" /home/kupiki/Kupiki-Hotspot-Admin-Backend/src/config/environment/development.js
+check_returned_code $?
+
 su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin-Backend && pm2 start --name backend npm -- start"
+check_returned_code $?
 
 git clone https://github.com/kupiki/Kupiki-Hotspot-Admin-Frontend.git
-chown -R kupiki:kupiki Kupiki-Hotspot-Admin-Frontend
-# Start packages installation with npm
-# cd Kupiki-Hotspot-Admin
-su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin-Frontend && export NODE_ENV= && npm install"
-# Rebuild node-sass for Raspberry Pi
-su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin-Frontend && npm rebuild node-sass"
-echo "
+check_returned_code $?
+
+echo '
 {
   "presets": [
     ["react"], ["env"]
   ],
   "plugins": ["transform-object-rest-spread"]
 }
-" > /home/kupiki/Kupiki-Hotspot-Admin-Frontend/.babelrc
+' > /home/kupiki/Kupiki-Hotspot-Admin-Frontend/.babelrc
+check_returned_code $?
+
+#chown -R kupiki:kupiki /home/kupiki/Kupiki-Hotspot-Admin-Frontend/.babelrc
+chown -R kupiki:kupiki /home/kupiki/Kupiki-Hotspot-Admin-Frontend
+check_returned_code $?
+
+# Start packages installation with npm
+# cd Kupiki-Hotspot-Admin
+su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin-Frontend && export NODE_ENV= && npm install"
+check_returned_code $?
+
+# Rebuild node-sass for Raspberry Pi
+su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin-Frontend && npm rebuild node-sass"
+check_returned_code $?
+
+sed -i "s/127.0.0.1/$MY_IP/g" /home/kupiki/Kupiki-Hotspot-Admin-Frontend/config/config.dev.json
+check_returned_code $?
 
 # Build project
 #su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin-Frontend && npm run-script build"
@@ -147,12 +191,20 @@ echo "
 #su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin-Frontend && pm2 start ecosystem.config.js --env production"
 
 su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin-Frontend && HOST='`ip -4 route get 8.8.8.8 | awk {'print $7'} | tr -d '\n'`' pm2 start --name frontend npm -- start"
+check_returned_code $?
 
 su - kupiki -c "cd /home/kupiki && pm2 save"
+check_returned_code $?
+
 # Add server as a service
 su - kupiki -c "pm2 startup systemd"
+#check_returned_code $?
+
 /usr/bin/pm2 startup systemd -u kupiki --hp /home/kupiki
+check_returned_code $?
+
 chmod -x /etc/systemd/system/pm2-kupiki.service
+check_returned_code $?
 
 # echo '
 # #!/bin/sh
@@ -167,35 +219,50 @@ chmod 700 /etc/kupiki
 
 cd /root
 git clone https://github.com/Kupiki/Kupiki-Hotspot-Admin-Backend-Script.git
+check_returned_code $?
+
 cp /root/Kupiki-Hotspot-Admin-Backend-Script/kupiki.sh /etc/kupiki/
+check_returned_code $?
+
 chmod 700 /etc/kupiki/kupiki.sh
+check_returned_code $?
 
 # remove all lines for kupiki in /etc/sudoers
 sed -i 's/^kupiki ALL.*//g' /etc/sudoers
+check_returned_code $?
+
 # update /etc/sudoers
 echo '
 kupiki ALL=(ALL) NOPASSWD:/etc/kupiki/kupiki.sh
 ' >> /etc/sudoers
+check_returned_code $?
 
-#echo '
-#su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin && pm2 stop 0"
-##su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin && git pull"
-#su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin && git fetch --all"
-#su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin && git reset --hard origin/master"
-#su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin && export NODE_ENV= && npm install"
-#su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin && gulp build"
-#su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin && pm2 start 0"
-#
-#cd /root/Kupiki-Hotspot-Admin-Script/
-#git pull
-#cp /root/Kupiki-Hotspot-Admin-Script/kupiki.sh /etc/kupiki/
-#chmod 700 /etc/kupiki/kupiki.sh
-#
-#' > /root/updateKupiki.sh
-#chmod +x /root/updateKupiki.sh
+echo '
+su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin && pm2 stop 0"
+#su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin && git pull"
+su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin && git fetch --all"
+su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin && git reset --hard origin/master"
+su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin && export NODE_ENV= && npm install"
+su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin && gulp build"
+su - kupiki -c "cd /home/kupiki/Kupiki-Hotspot-Admin && pm2 start 0"
+
+cd /root/Kupiki-Hotspot-Admin-Script/
+git pull
+cp /root/Kupiki-Hotspot-Admin-Script/kupiki.sh /etc/kupiki/
+chmod 700 /etc/kupiki/kupiki.sh
+
+' > /root/updateKupiki.sh
+check_returned_code $?
+
+chmod +x /root/updateKupiki.sh
+check_returned_code $?
 
 apt-get install -y freeradius-utils
+check_returned_code $?
+
 sed -i "s/^#LoadPlugin unixsock/LoadPlugin unixsock/" /etc/collectd/collectd.conf
+check_returned_code $?
+
 echo '
 <Plugin unixsock>
         SocketFile "/var/run/collectd-unixsock"
@@ -203,8 +270,10 @@ echo '
         SocketPerms "0660"
         DeleteSocket false
 </Plugin>' >> /etc/collectd/collectd.conf
+check_returned_code $?
 
 service collectd restart
+check_returned_code $?
 
 # To reset local developments
 
